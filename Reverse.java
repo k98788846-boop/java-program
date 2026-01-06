6443b7ad5e8facfe40ed57b0248e6d041d9d55af
k98788846-boop <k98788846@gmail.com>
2026-01-06 07:56:19 +0530

    reverse in single aray (swap)

--- Reverse.java
+++ Reverse.java
@@ -3,14 +3,16 @@ public class Reverse{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int a[]=new int[n];int b[]=new int[n];
-        for(int i=0;i<n;i++){
+        int a[]=new int[n];for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
-            b[i]=a[i];
+        }
+        for(int i=0;i<n/2;i++){
+           int temp=a[i];
+           a[i]=a[n-1-i];
+           a[n-1-i]=temp;
         }
         for(int i=0;i<n;i++)
-        System.out.println("copy of array"+b[i]);
+            System.out.println("reverse"+a[i]);
         
-
-        }
+    }
 }
