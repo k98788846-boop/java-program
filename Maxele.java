2c60396a481ea33d522fbdc78c37c4e93d118e13
k98788846-boop <k98788846@gmail.com>
2026-01-06 07:29:52 +0530

    s=a[0]

--- Maxele.java
+++ Maxele.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Maxele{ 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);int n=sc.nextInt();
-        int a[]=new int[n];int s=0;
+        int a[]=new int[n];int s=a[0];
         for(int i=0;i<n;i++){           
             a[i]=sc.nextInt();
             if(s<=a[i]){
