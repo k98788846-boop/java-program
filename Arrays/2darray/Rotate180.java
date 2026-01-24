6f4a43b534ac52bd6333382d611e09382bbfc2a7
k98788846-boop <k98788846@gmail.com>
2026-01-24 11:18:09 +0530

    roate 180 array clockwise

---
+++ Arrays/2darray/Rotate180.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Rotate180 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int a[][]=new int[r][r];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<r;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<r/2;i++){
+            for(int j=0;j<r;j++){
+               
+              int temp=a[i][j];
+              a[i][j]=a[r-1-i][j];
+              a[r-1-i][j]=temp;// s1: interchange rows .s2;reverse row wise//
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<r/2;j++){
+                int temp =a[i][j];
+                a[i][j]=a[i][r-1-j];
+                a[i][r-1-j]=temp;
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<r;j++){
+                System.out.print(a[i][j]);
+            }
+        }
+        sc.close();
+    }
+}
