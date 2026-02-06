126c63248d69d8d9e8e9c6de9cad22c9ec5d75bb
k98788846-boop <k98788846@gmail.com>
2026-02-06 20:09:46 +0530

    sum of n elemnts

---
+++ Arrays/Recursive/Sum.java
@@ -0,0 +1,17 @@
+package Arrays.Recursive;
+
+import java.util.Scanner;
+public class Sum{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(sum(n));
+        sc.close();
+    }
+    static int sum(int n){
+         if(n==0){
+            return n;
+        }
+        return n + sum(n-1);
+    } 
+}
