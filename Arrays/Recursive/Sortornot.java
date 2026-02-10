6cebd43bae3621d3ade77b7d6f8fb6726eb4bcaf
k98788846-boop <k98788846@gmail.com>
2026-02-10 18:43:43 +0530

    find sort or not

---
+++ Arrays/Recursive/Sortornot.java
@@ -0,0 +1,16 @@
+package Arrays.Recursive;
+
+import java.util.Scanner;
+public class Sortornot {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a[]={1,2,3,5,4};
+        System.out.print(sort(a,0));
+    }
+    static boolean sort(int a[],int index){
+        if(index==(a.length-1)){
+            return true;
+        }
+        return a[index] <a[index+1] && sort(a,index+1);
+    }
+}
