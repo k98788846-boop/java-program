3a2dd5b1b8ed6b6e8d8fdd5883827413a01293c6
k98788846-boop <k98788846@gmail.com>
2026-02-10 22:06:37 +0530

    frequency of element

---
+++ Arrays/Recursive/Frequencyarr.java
@@ -0,0 +1,25 @@
+package Arrays.Recursive;
+
+import java.util.Scanner;
+public class Frequencyarr {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a[]={1,2,2,3,4,4,2};int ele=2;
+        int count=0;
+        System.out.print(frequency(a,count,ele,0));
+        sc.close();
+    }
+    static int frequency(int a[],int count,int ele,int index){
+        if(index==a.length-1){
+            if(a[index]==ele){
+                count++;
+            }
+            return count;
+        }        
+        else if(a[index]==ele){
+            count++;
+        }
+        return frequency(a,count,ele,index+1);
+
+    }
+}
