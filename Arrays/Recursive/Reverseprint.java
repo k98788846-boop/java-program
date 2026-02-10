d53f337684b5a8c8e232e84135c9fe6ef0cec890
k98788846-boop <k98788846@gmail.com>
2026-02-10 22:07:32 +0530

    print array in reverse

---
+++ Arrays/Recursive/Reverseprint.java
@@ -0,0 +1,20 @@
+package Arrays.Recursive;
+
+import java.util.Scanner;
+public class Reverseprint {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a[]={1,2,3,4,5,6};
+        print(a,a.length-1);
+        sc.close();
+    }
+    static void print(int a[],int index){
+        if(index==0){
+            System.out.print(a[index]);
+            return;
+        }
+        System.out.print(a[index]);
+        print(a,index-1);
+        return;
+    }
+}
