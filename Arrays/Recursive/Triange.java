aa4c8cf2764a24c9f7fef6d4a27c16460698a64a
k98788846-boop <k98788846@gmail.com>
2026-02-14 13:11:29 +0530

    triangle

---
+++ Arrays/Recursive/Triange.java
@@ -0,0 +1,23 @@
+package Arrays.Recursive;
+import java.util.Scanner;
+public class Triange {
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    triangle(4,0);
+}
+static void triangle(int r,int c){
+    if(r==0){
+        return;
+    }
+    if(r>c){
+         System.out.print("*");
+         triangle(r,c+1);
+         return;
+    }else{
+        System.out.println();
+        triangle(r-1,0);
+        return;
+    }
+
+    }
+}
